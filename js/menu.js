6fb9adb57858e9c5dbaab76eabec0922694700a5
Tyson Truong <tystruong@gmail.com>
2021-11-30 11:21:22 +1100

    Readjusted styles for about page, added mobile styles

--- js/menu.js
+++ js/menu.js
@@ -3,6 +3,9 @@
 let emojis = ["1F44B", "1F44F", "1F44A", "1F44C"];
 let emojiCount = 0;
 
+let role_emoji = ["1F4DA", "1F393", "1F3A8"];
+let role = ["Tutor", "Artist", "Student"];
+let roleCount = 0;
 
 function toggleMenu() {
     let menu = document.querySelector(".menu");
@@ -21,4 +24,18 @@ function cycleEmoji() {
 
     emojiRef.innerHTML = String.fromCodePoint("0x" + emojis[emojiCount]);
 
+}
+
+function cycleRole() {
+    let emojiRef = document.getElementById("role-emoji");
+    let roleRef = document.getElementById("role-span");
+
+    if (roleCount == role.length - 1) {
+        roleCount = 0;
+    } else {
+        roleCount++;
+    }
+
+    emojiRef.innerHTML = String.fromCodePoint(`0x${role_emoji[roleCount]}`);
+    roleRef.innerHTML = role[roleCount];
 }
